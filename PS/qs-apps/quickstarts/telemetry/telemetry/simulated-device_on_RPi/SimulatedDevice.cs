d72685d1d50506052f9672b770a855ca3fed63d6
David Jones <davidjones@sportronics.com.au>
2020-03-15 14:47:13 +1100

    Now is a RPi using DHT22 app under telemetry

--- PS/qs-apps/quickstarts/telemetry/telemetry/simulated-device_on_RPi/SimulatedDevice.cs
+++ PS/qs-apps/quickstarts/telemetry/telemetry/simulated-device_on_RPi/SimulatedDevice.cs
@@ -10,9 +10,8 @@ using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 
-using Iot.Device.CpuTemperature;
+//using Iot.Device.CpuTemperature;
 using System.Device.I2c;
-using Iot.Device.Bmp180;
 using Iot.Device.DHTxx;
 using System.Threading;
 
@@ -46,140 +45,121 @@ namespace simulated_device
             double minTemperature = 20;
             double minHumidity = 60;
             Random rand = new Random();
-            CpuTemperature _temperature = null;
-            Dht11 dht = null;
-            Bmp180 i2cBmp280 = null;
+            //CpuTemperature _temperature = null;
+            Dht22 dht = null;
+            //Bmp180 i2cBmp280 = null;
+            Console.WriteLine ("Using Sensor {0}",Sensor );
             try {
-                if((Sensor == Sensors.BMP280) || (Sensor == Sensors.BME280))
-                {
-                    _temperature = new CpuTemperature();
-                    Console.WriteLine("Using harBMP280 or BPE280");   
-                }
-                else if(Sensor == Sensors.DHT11)
+               if(Sensor == Sensors.DHT22)
                 {
                     //1-Wire:
                     int  pin = 26;
-                    dht = new Dht11(pin);
-                    Console.WriteLine("Using DHT11");   
-                }
-                else if (Sensor == Sensors.BMP180)
-                {                 
-                    // bus id on the raspberry pi 3
-                    const int busId = 1;
-                    var i2cSettings = new I2cConnectionSettings(busId, Bmp180.DefaultI2cAddress);
-                    var i2cDevice = I2cDevice.Create(i2cSettings);
-                    i2cBmp280 = new Bmp180(i2cDevice);
-                    i2cBmp280.SetSampling(Sampling.Standard);
-                    Console.WriteLine("Using BMP100");   
+                    dht = new Dht22(pin);
+                    Console.WriteLine("Using DHT22");   
                 }
                 else{
                     Console.WriteLine("Not using hardware");
                 }
 
-                //Only DH12 can use DH12:
-                /*I2cConnectionSettings settings = new I2cConnectionSettings(1, DhtSensor.DefaultI2cAddressDht12);
-                I2cDevice device = I2cDevice.Create(settings);
-                Dht12 dht12 = new Dht12(device);*/
+  
                 
             }
             catch (Exception)
             {
                 Console.WriteLine("No hardware");
-                _temperature = null;
+                //_temperature = null;
                  dht = null;
-                i2cBmp280 = null;
+                //i2cBmp280 = null;
             }
 
             while (true)
             {
+                bool lastResult=true;
                 double currentTemperature = minTemperature + rand.NextDouble() * 15;
                 double currentHumidity = minHumidity + rand.NextDouble() * 20;
-
-                if (_temperature != null)
-                {
-                    if (_temperature.IsAvailable)
-                    {
-                        Console.WriteLine($"The CPU temperature is {Math.Round(_temperature.Temperature.Celsius, 2)}");
-                        currentTemperature = _temperature.Temperature.Celsius;
-                        if (Sensor == Sensors.BME280)
-                        {
-                            //currentHumidity = _temperature.Humidity;
-                        }
-
-                    }
-                }
-                else if (dht != null){
+                if (dht != null){
                     currentTemperature = (double) dht.Temperature.Celsius;
+                    bool result1 = dht.IsLastReadSuccessful;
                     currentHumidity = dht.Humidity;
+                    bool result2 = dht.IsLastReadSuccessful;
+                    if (!result1 || !result2) 
+                    {
+                        Console.Write(".");
+                        lastResult = false;
+                    }
+                    else
+                    {
+                        //Sanity Check
+                        bool resultIsValid = true;
+                        if (currentTemperature is double.NaN)
+                            resultIsValid = false;
+                        else if (currentHumidity is double.NaN)
+                            resultIsValid = false;
+                        if (!resultIsValid)
+                        {
+                            Console.Write("#");
+                            lastResult = false;
+                        }
+                        else
+                        {
+                            bool resultIsSane = true;
+                            if ((currentTemperature> 100) || (currentTemperature< -20))
+                                resultIsSane = false;
+                            else if ((currentHumidity> 100) || (currentHumidity> 100))
+                                resultIsSane = false;
+                            if (!resultIsSane)
+                            {
+                                Console.Write("x");
+                                lastResult = false;
+                            }
+                            else
+                            {
+
+                                if (!lastResult)
+                                    Console.WriteLine("");
+                                lastResult = true;
+                                currentTemperature = Math.Round(currentTemperature,4);
+                                currentHumidity = Math.Round(currentHumidity,4);
+                                Console.Write($"Temperature: {currentTemperature.ToString("0.0")} °C ");
+                                Console.Write($"Humidity: { currentHumidity.ToString("0.0")} % ");
+                                Console.WriteLine("");
+                            }
+                        }
+                    }                   
                 }
-                else if (i2cBmp280 != null){
-                    currentTemperature = (double) i2cBmp280.ReadTemperature().Celsius;
-                    //currentHPressure = (double) i2cBmp280.ReadPressure().Hectopascal;
+             
+                if (lastResult){
+                    // Create JSON message
+                    var telemetryDataPoint = new
+                    {
+                        temperature = currentTemperature,
+                        humidity = currentHumidity
+                    };
+                    var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                    // Add a custom application property to the message.
+                    // An IoT hub can filter on these properties without access to the message body.
+                    message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                    // Send the telemetry message
+                    await s_deviceClient.SendEventAsync(message);
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
                 }
-               /* else if (dht12 != null){
-                    currentTemperature = (double)dht12.Temperature.Celsius;
-                    currentHumidity = dht12.Humidity;
-                }*/
 
-                // Create JSON message
-                var telemetryDataPoint = new
-                {
-                    temperature = currentTemperature,
-                    humidity = currentHumidity
-                };
-                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                var message = new Message(Encoding.ASCII.GetBytes(messageString));
-
-                // Add a custom application property to the message.
-                // An IoT hub can filter on these properties without access to the message body.
-                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
-
-                // Send the telemetry message
-                await s_deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
-
-                await Task.Delay(1000);
+                await Task.Delay(2000);
             }
         }
 
-        private enum Sensors  {none,DHT11,BMP180,BMP280,BME280};
+        private enum Sensors  {none,DHT11,DHT22,BMP180,BMP280,BME280};
         private static Sensors Sensor =    Sensors.none;
         private static void Main(string[] args)
         {
-            if (SensorIn != null);
-            {
-                SensorIn = SensorIn.ToUpper();
-                if (SensorIn != "")
-                {
-                    if (SensorIn == "BMP180")
-                    {
-                        //Ref: https://github.com/dotnet/iot/tree/master/src/devices/Bmp180
-
-
-                    }
-                    else{
-                        switch (SensorIn.Substring(0,3))
-                        {
-                            case "DHT":
-                            //Temp and Pressure
-                            Sensor = Sensors.DHT11;
-                            break;
-                            case "BMP":
-                            //Temp and Pressure
-                            Sensor = Sensors.BMP280;
-                            break;  
-                            case "BME":
-                            //Temp Pressure and Humidity
-                            Sensor = Sensors.BME280;
-                            break;
-                        }
-                    }
-
-                }
-            }
+            Sensor = Sensors.DHT22;
 
             Console.WriteLine("IoT Hub Quickstarts #1 - Simulated device. Ctrl-C to exit.\n");
             Console.WriteLine ("Using Env Var IOTHUB_DEVICE_CONN_STRING = " + s_connectionString );
+            Console.WriteLine ("Using Sensor {0}",Sensor );
 
             // Connect to the IoT hub using the MQTT protocol
             s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, TransportType.Mqtt);
