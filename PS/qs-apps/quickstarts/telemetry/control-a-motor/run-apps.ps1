50b854fe2c6bd3297c74b8a5ace1baf98317f9e3
David Jones <davidjones@sportronics.com.au>
2020-03-24 18:29:52 +1100

    telem motor works on IoT-core

--- PS/qs-apps/quickstarts/telemetry/control-a-motor/run-apps.ps1
+++ PS/qs-apps/quickstarts/telemetry/control-a-motor/run-apps.ps1
@@ -1,6 +1,4 @@
-set-location simulated-device-2
-Start-process dotnet run
-set-location ..\back-end-application
+set-location .\back-end-application
 start-process dotnet run
 set-location ..\read-d2c-messages
 start-process dotnet run
